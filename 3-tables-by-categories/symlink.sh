8041fe9b3e91dd5dd5a4f0bf7aa31f5ffe0a4f8f
Karel Břinda <karel.brinda@gmail.com>
2020-04-12 22:02:07 -0400

    Improve symlinks for cases

--- 3-tables-by-categories/symlink.sh
+++ 3-tables-by-categories/symlink.sh
@@ -4,10 +4,16 @@ set -e
 set -o pipefail
 set -u
 
-rm -f *.tsv
-grep -n 'Berkshire' ../*.tsv \
+rm -f */*.tsv
+(
+mkdir -p 1_cases
+cd 1_cases
+grep -n 'Berkshire' ../../2-*/*.tsv \
 	| grep -Evi '(male|female)' \
 	| perl -pe 's/:.*//g' \
 	| sort \
 	| uniq \
 	| xargs -L1 ln -s
+
+rename 's/table\d+\.//g' *.tsv
+)
